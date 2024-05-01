a53d31ed986f4d9258e11d5b8c44ffc16a82f8cb
fakeITDevTeam <dev5544455@gmail.com>
2024-05-01 11:11:39 +0700

    design signup form, verify email and success screen

--- lib/features/authentication/screens/signup/widgets/signup_form.dart
+++ lib/features/authentication/screens/signup/widgets/signup_form.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_emboss_store/features/authentication/screens/signup/widgets/terms_conditions_checkbox.dart';
+import 'package:flutter_emboss_store/features/authentication/screens/signup/widgets/verify_email.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 import '../../../../../utils/constants/sizes.dart';
@@ -91,7 +93,7 @@ class TSignupForm extends StatelessWidget {
           SizedBox(
             width: double.infinity,
             child: ElevatedButton(
-              onPressed: () {},
+              onPressed: () => Get.to(() => const VerifyEmailScreen()),
               child: const Text(TTexts.createAccount),
             ),
           ),
