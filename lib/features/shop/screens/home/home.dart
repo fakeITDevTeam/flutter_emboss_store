c7e7a48d1d2b9300749bc318feebeaff3dc2dde8
fakeITDevTeam <dev5544455@gmail.com>
2024-05-03 20:17:41 +0700

    design: create custom-shape for app screens

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +7,15 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TPrimaryHeaderContainer(
+              // child: Container(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
