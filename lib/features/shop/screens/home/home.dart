eead73a0bf2879ed8f844ae522a0d186f5366a58
fakeITDevTeam <dev5544455@gmail.com>
2024-05-05 13:12:41 +0700

    design: edit: create THomeAppBar from TAppBar

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,41 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_emboss_store/common/widgets/appbar/appbar.dart';
-import 'package:flutter_emboss_store/utils/constants/colors.dart';
-import 'package:flutter_emboss_store/utils/constants/text_strings.dart';
+import 'package:flutter_emboss_store/features/shop/screens/home/widgets/home_appbar.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
-import '../../../../common/widgets/products/cart/cart_menu_icon.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             TPrimaryHeaderContainer(
               child: Column(
                 children: [
-                  TAppBar(
-                    title: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          TTexts.homeAppbarTitle,
-                          style: Theme.of(context).textTheme.labelMedium!.apply(color: TColors.grey),
-                        ),
-                        Text(
-                          TTexts.homeAppbarSubTitle,
-                          style: Theme.of(context).textTheme.headlineSmall!.apply(color: TColors.white),
-                        ),
-                      ],
-                    ),
-                    actions: [
-                      TCartCounterIcon(onPressed: () {}, iconColor: TColors.white,),
-                    ],
-                  ),
+                  THomeAppBar(),
                 ],
               ),
             ),
@@ -45,3 +25,4 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
