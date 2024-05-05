ad4ffff7b075014d4ec0e0812bde2c23c93b5afd
fakeITDevTeam <dev5544455@gmail.com>
2024-05-05 12:26:25 +0700

    design: edit: eliminate unused imports

--- lib/common/widgets/custom_shapes/containers/primary_header_container.dart
+++ lib/common/widgets/custom_shapes/containers/primary_header_container.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 import '../../../../utils/constants/colors.dart';
 import '../curved_edges/curved_edges_widget.dart';
