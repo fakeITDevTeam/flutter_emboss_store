77089be3915ce4283b915cd6f7d78105f12e2b6b
fakeITDevTeam <dev5544455@gmail.com>
2024-05-05 12:34:31 +0700

    design: edit: simplified code by using widgets inside TPrimaryHeaderContainer: TCartCounterIcon

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_emboss_store/common/widgets/appbar/appbar.dart';
+import 'package:flutter_emboss_store/utils/constants/colors.dart';
+import 'package:flutter_emboss_store/utils/constants/text_strings.dart';
+
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+import '../../../../common/widgets/products/cart/cart_menu_icon.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             TPrimaryHeaderContainer(
-              // child: Container(),
+              child: Column(
+                children: [
+                  TAppBar(
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          TTexts.homeAppbarTitle,
+                          style: Theme.of(context).textTheme.labelMedium!.apply(color: TColors.grey),
+                        ),
+                        Text(
+                          TTexts.homeAppbarSubTitle,
+                          style: Theme.of(context).textTheme.headlineSmall!.apply(color: TColors.white),
+                        ),
+                      ],
+                    ),
+                    actions: [
+                      TCartCounterIcon(onPressed: () {}, iconColor: TColors.white,),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
