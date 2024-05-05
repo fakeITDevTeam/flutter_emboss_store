f5fbd9372a3a3f11bffc5cad7e65128db96b79a1
fakeITDevTeam <dev5544455@gmail.com>
2024-05-05 12:25:11 +0700

    design: edit: wrapped stack with sized-box to overcome the custom shape not appearing

--- lib/common/widgets/custom_shapes/containers/primary_header_container.dart
+++ lib/common/widgets/custom_shapes/containers/primary_header_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import '../../../../utils/constants/colors.dart';
 import '../curved_edges/curved_edges_widget.dart';
@@ -7,19 +8,18 @@ import 'circular_container.dart';
 class TPrimaryHeaderContainer extends StatelessWidget {
   const TPrimaryHeaderContainer({
     super.key,
-    // required this.child,
+    required this.child,
   });
 
-  // final Widget child;
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
     return TCurvedEdgesWidget(
-      child: Container(
-        color: TColors.primary,
-        padding: const EdgeInsets.only(bottom: 0),
-        child: SizedBox(
-          height: 400,
+      child: SizedBox(
+        height: 400,
+        child: Container(
+          color: TColors.primary,
           child: Stack(
             children: [
               /// background custom shapes
@@ -35,7 +35,7 @@ class TPrimaryHeaderContainer extends StatelessWidget {
                   backgroundColor: TColors.textWhite.withOpacity(0.1),
                 ),
               ),
-              // child,
+              child,
             ],
           ),
         ),
