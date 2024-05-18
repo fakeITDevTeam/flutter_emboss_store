e01971f9f217a3188da38927abf916e2a893fcf6
fakeITDevTeam <dev5544455@gmail.com>
2024-05-18 20:02:38 +0700

    widget: modified: clear backgroundColor property, align icontitle to centered

--- lib/common/widgets/image_text_widgets/vertical_image_text.dart
+++ lib/common/widgets/image_text_widgets/vertical_image_text.dart
@@ -10,7 +10,7 @@ class TVerticalImageText extends StatelessWidget {
     required this.image,
     required this.title,
     this.textColor = TColors.white,
-    this.backgroundColor = TColors.white,
+    this.backgroundColor,
     this.onTap,
   });
 
@@ -49,6 +49,7 @@ class TVerticalImageText extends StatelessWidget {
             SizedBox(
               width: 55,
               child: Text(title,
+                textAlign: TextAlign.center,
                 style: Theme.of(context).textTheme.labelMedium!.apply(color: textColor),
                 maxLines: 1,
                 overflow: TextOverflow.ellipsis,
