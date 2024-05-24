1336f0175b88c890ada254a3645188a8989eed0c
fakeITDevTeam <dev5544455@gmail.com>
2024-05-24 20:54:30 +0700

    widget: new: create TBrandCart

---
+++ lib/common/widgets/brands/brand_card.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/constants/colors.dart';
+import '../../../utils/constants/enums.dart';
+import '../../../utils/constants/image_strings.dart';
+import '../../../utils/constants/sizes.dart';
+import '../../../utils/helpers/helper_functions.dart';
+import '../custom_shapes/containers/rounded_container.dart';
+import '../images/circular_image.dart';
+import '../texts/brand_title_text_with_verified_icon.dart';
+
+class TBrandCard extends StatelessWidget {
+  const TBrandCard({
+    super.key,
+    required this.showBorder,
+    this.onTap,
+  });
+
+  final bool showBorder;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = THelperFunctions.isDarkMode(context);
+    return GestureDetector(
+      onTap: onTap,
+      child: TRoundedContainer(
+        padding: const EdgeInsets.all(TSizes.sm),
+        showBorder: showBorder,
+        backgroundColor: Colors.transparent,
+        child: Row(
+          children: [
+            /// icon
+            Flexible(
+              child: TCircularImage(
+                isNetworkImage: false,
+                image: TImages.clothIcon,
+                backgroundColor: Colors.transparent,
+                overlayColor: isDark ? TColors.white : TColors.black,
+              ),
+            ),
+            const SizedBox(width: TSizes.spaceBtwItems / 2,),
+
+            /// text
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const TBrandTitleTextWithVerifiedIcon(title: 'Nike', brandTextSize: TextSizes.large,),
+                  Text('256 products', overflow: TextOverflow.ellipsis, style: Theme.of(context).textTheme.labelMedium,),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
